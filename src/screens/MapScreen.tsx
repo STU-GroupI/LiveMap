fb60f65e2137569fb5a566518ca0bb6979c1d3f5
JrnJ <jeroenjochems@outlook.com>
2025-03-20 14:03:44 +0100

    banaan

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
+import { StyleSheet, TouchableOpacity, View, Text, Image } from 'react-native';
+import {Icon, Button} from 'react-native-paper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import {useMapConfig} from '../config/MapConfigContext.tsx';
 import useLocation from '../hooks/UseLocation.tsx';
 import Loader from '../components/Loader.tsx';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
-import {Camera, MapView, PointAnnotation, UserLocation} from '@maplibre/maplibre-react-native';
-import {Icon} from 'react-native-paper';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+import {Camera, MapView, UserLocation} from '@maplibre/maplibre-react-native';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
 const MapScreen = () => {
     const {
@@ -18,49 +22,81 @@ const MapScreen = () => {
     const { hasPermission } = useLocation();
     const insets = useSafeAreaInsets();
 
+    // ref
+    const bottomSheetRef = useRef<BottomSheet>(null);
+
+    // callbacks
+    const handleSheetChanges = useCallback((index: number) => {
+        console.log('handleSheetChanges', index);
+    }, []);
+
     if (loading || !hasPermission) {
         return <Loader />;
     }
 
     return (
         <View style={styles.container}>
-            <MapView
-                style={styles.map}
-                mapStyle={config.mapStyle}
-            >
-                <Camera
-                    ref={cameraRef}
-                    centerCoordinate={config.center}
-                />
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <MapView
+                    style={styles.map}
+                    mapStyle={config.mapStyle}
+                >
+                    <Camera
+                        ref={cameraRef}
+                        centerCoordinate={config.center}
+                    />
 
-                {pois.map((poi, idx) => (
-                    <PointAnnotation
-                        key={idx}
-                        id={idx.toString()}
-                        coordinate={[poi.longitude, poi.latitude]}
-                        anchor={{ x: 0.45, y: 0.9 }}
-                        onSelected={() => console.log('POI selected:', poi)}
-                    >
-                        <Icon size={30} source="map-marker" />
-                    </PointAnnotation>
-                ))}
+                    <UserLocation showsUserHeadingIndicator={true}/>
+                </MapView>
 
-                <UserLocation showsUserHeadingIndicator={true}/>
-            </MapView>
+                <View
+                    style={[
+                        styles.controls,
+                        {
+                            top: insets.top + 10,
+                            right: insets.right + 10,
+                        },
+                    ]}
+                >
+                    <TouchableOpacity style={styles.button} onPress={handleRecenter} activeOpacity={0.9}>
+                        <Icon source="crosshairs-gps" size={26} color="#000" />
+                    </TouchableOpacity>
+                </View>
 
-            <View
-                style={[
-                    styles.controls,
-                    {
-                        top: insets.top + 10,
-                        right: insets.right + 10,
-                    },
-                ]}
-            >
-                <TouchableOpacity style={styles.button} onPress={handleRecenter}>
-                    <Icon source="crosshairs-gps" size={26} color="#000" />
-                </TouchableOpacity>
-            </View>
+                <BottomSheet
+                    ref={ bottomSheetRef }
+                    onChange={ handleSheetChanges }
+                    snapPoints={['40%', '70%']}
+                >
+                    <BottomSheetView style={styles.modalContentContainer}>
+                        <View style={styles.modalImageContainer}>
+                            <View style={{ width: '100%', overflow: 'hidden' }}>
+                                <Image 
+                                    style={{ width: '100%', height: 200, resizeMode: 'cover', borderTopLeftRadius: 16, borderTopRightRadius: 16 }}
+                                    source={require('../images/plus-supermarket.png')} />
+                            </View>
+                            <TouchableOpacity style={styles.modalReportButton} activeOpacity={0.8}>
+                                <Icon source="alert-circle-outline" size={40} color="#fff" />
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text style={{ fontSize: 32 }}>Supermarkt Landal Plus</Text>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={{ fontWeight: 'bold' }}>4,5</Text>
+                            <Icon source="star" size={16} color="#FFBB00" />
+                            <Icon source="star" size={16} color="#FFBB00" />
+                            <Icon source="star" size={16} color="#FFBB00" />
+                            <Icon source="star" size={16} color="#FFBB00" />
+                            <Icon source="star" size={16} color="#FFBB00" />
+                        </View>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 12 }}>Body Small</Text>
+                            <Text style={{ fontSize: 12 }}> • </Text>
+                            <Icon source="wheelchair-accessibility" size={16} color="#000" />
+                        </View>
+                    </BottomSheetView>
+                </BottomSheet>
+            </GestureHandlerRootView>
         </View>
     );
 };
@@ -83,6 +119,30 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+
+    modalContentContainer: {
+        flex: 1,
+        borderRadius: 16,
+        backgroundColor: '#fff',
+    },
+    modalImageContainer: {
+        position: 'relative',
+        width: '100%',
+        backgroundColor: '#fff',
+    },
+    modalReportButton: {
+        position: 'absolute',
+        top: 16,
+        right: 16,
+
+        backgroundColor: 'tomato',
+        width: 50,
+        height: 50,
+        borderRadius: '100%',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
 
 export default MapScreen;
